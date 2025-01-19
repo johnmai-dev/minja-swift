47b291a044de3f83e58daa1d2c4498c0d43f3701
John Mai <maiqingqiang@foxmail.com>
2025-01-19 18:34:54 +0800

    :art: format code

--- Package.swift
+++ Package.swift
@@ -4,49 +4,49 @@
 import PackageDescription
 
 let package = Package(
-    name: "minja-swift",
-    platforms: [.macOS(.v14)],
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "Minja",
-            targets: ["Minja"]
-        )
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "Cminja",
-            exclude: [
-                "minja"
-            ]
-        ),
-        .target(
-            name: "Minja",
-            dependencies: [
-                "Cminja",
-            ],
-            swiftSettings: [
-                .interoperabilityMode(.Cxx)
-            ]
-        ),
-        .testTarget(
-            name: "MinjaTests",
-            dependencies: [
-                "Minja",
-            ],
-            swiftSettings: [
-                .interoperabilityMode(.Cxx)
-            ]
-        ),
-        .testTarget(
-            name: "CminjaTests",
-            dependencies: ["Cminja"],
-            swiftSettings: [
-                .interoperabilityMode(.Cxx)
-            ]
-        )
-    ],
-    cxxLanguageStandard: .gnucxx17
+  name: "minja-swift",
+  platforms: [.macOS(.v14)],
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "Minja",
+      targets: ["Minja"]
+    )
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+      name: "Cminja",
+      exclude: [
+        "minja"
+      ]
+    ),
+    .target(
+      name: "Minja",
+      dependencies: [
+        "Cminja"
+      ],
+      swiftSettings: [
+        .interoperabilityMode(.Cxx)
+      ]
+    ),
+    .testTarget(
+      name: "MinjaTests",
+      dependencies: [
+        "Minja"
+      ],
+      swiftSettings: [
+        .interoperabilityMode(.Cxx)
+      ]
+    ),
+    .testTarget(
+      name: "CminjaTests",
+      dependencies: ["Cminja"],
+      swiftSettings: [
+        .interoperabilityMode(.Cxx)
+      ]
+    ),
+  ],
+  cxxLanguageStandard: .gnucxx17
 )
