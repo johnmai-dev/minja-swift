6aabae7c124be44ea65fae8a94b474ea699c187a
John Mai <maiqingqiang@foxmail.com>
2025-01-19 18:34:57 +0800

    :art: format code

--- Tests/CminjaTests/CminjaTests.swift
+++ Tests/CminjaTests/CminjaTests.swift
@@ -5,22 +5,21 @@
 //  Created by John Mai on 2025/1/18.
 //
 
-
+import CxxStdlib
 import Testing
 
 @testable import Cminja
-import CxxStdlib
 
 struct CminjaTests {
-    @Test
-    func test() throws {
-//        var template:minja.TemplateNode = minja.Parser.parse("", minja.Options()).pointee
-//        
-//        var value = minja.Value(std.string(""))
-//        
-//        
-//        var ctx = minja_context_make(value)
-//                
-//        template.render(ctx)
-    }
+  @Test
+  func test() throws {
+    //        var template:minja.TemplateNode = minja.Parser.parse("", minja.Options()).pointee
+    //
+    //        var value = minja.Value(std.string(""))
+    //
+    //
+    //        var ctx = minja_context_make(value)
+    //
+    //        template.render(ctx)
+  }
 }
